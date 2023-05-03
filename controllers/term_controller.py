24899f122ae53d801a07e9ae5c82752b38d7e1c6
Paulo Cabral <paulo.luiz127@gmail.com>
2023-05-03 00:21:58 -0300

    .

--- controllers/term_controller.py
+++ controllers/term_controller.py
@@ -17,6 +17,9 @@ def get_terms(user: UserResModel = Depends(auth.get_user_logged)):
 def delete_term_by_id(id: int, user: UserResModel = Depends(auth.get_user_logged)):
     session = Session()
     term = session.query(Term).filter(Term.id == id).first()
+    user_term = session.query(UserTerm).filter(UserTerm.term_id == id).first()
+    session.delete(user_term)
+    session.commit()
     session.delete(term)
     session.commit()
     return {"success": True, "message": "Termo deletado com sucesso!"}
@@ -32,10 +35,12 @@ def create_term(model: TermReqModel, user: UserResModel = Depends(auth.get_user_
         session.add(term)
         session.commit()
 
-    user_term = UserTerm(user.id, term.id)
+    user_term = session.query(UserTerm).filter(UserTerm.term_id == term.id).first()
 
-    session.add(user_term)
-    session.commit()
+    if not user_term:
+        user_term = UserTerm(user.id, term.id)
+        session.add(user_term)
+        session.commit()
 
     return {"success": True, "message": "Termo criado com sucesso!"}
 
